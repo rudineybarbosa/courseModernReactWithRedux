67c2df8fbf31e69fd2f0c04c2a029757d3a6dc3f
rudineybarbosa <rudiney1h@gmail.com>
2022-03-28 14:16:15 -0300

    projeto youtube

---
+++ section11/videos/src/api/youtube.js
@@ -0,0 +1,2 @@
+const KEY = 'AIzaSyDS7zIRqTgU8w_SvKcshL4xx_89_GjhN-8'
+
---
+++ section11/videos/src/components/SearchBar.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+class SearchBar extends React.Component{
+
+			state = {term:''};
+
+	onInputChange = (event) => {
+		this.setState({term: event.target.value})
+	};
+
+	onFormSubmit = (event) => {
+		event.preventDefault();
+
+		//todo pass callback to parent componenent
+	};
+
+
+	render(){
+		return (
+			<div className="search-bar ui segment">
+				<form className="ui form" onSubmit={this.onFormSubmit}>
+					<div className="field">
+						<label>Video Search</label>
+						<input type='text'
+							onChange={this.onInputChange}
+							value={this.state.term}/>
+					</div>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default SearchBar;
